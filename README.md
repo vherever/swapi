031dedbda142cf6cff9457c027ed699723a0c5e7
vherever <vherever@gmail.com>
2018-04-15 18:39:29 +0300

    routing bug fix

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 **How to start**
 1. npm install
 2. ng serve
+3. navigate http://localhost:4200/
 
 **To do**
 1. Load all species and filter people by race, classification, etc
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { CharacterListComponent } from './character-list/character-list.component';
 import { DetailsComponent } from './details/details.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: CharacterListComponent
+    redirectTo: 'people',
+    pathMatch: 'full'
   },
   {
     path: 'people',
-    component: CharacterListComponent
+    component: DetailsComponent
   },
   {
     path: 'people/:id',
